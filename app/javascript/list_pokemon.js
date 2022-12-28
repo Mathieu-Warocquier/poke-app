6752afbd0aa794c03e711588051919ba7df0bd07
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-12-28 11:28:24 +0100

    fix list

--- app/javascript/list_pokemon.js
+++ app/javascript/list_pokemon.js
@@ -1,19 +1,17 @@
 const results = document.querySelector("#list")
 
-fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
+fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
   .then(response => response.json())
   .then((data) => {
     data.results.forEach((pokemon) => {
-      // console.log(pokemon.url)
-
 
       fetch(pokemon.url)
         .then(response => response.json())
         .then((data) => {
 
           const listPokemon =
-            `<li class="list-inline-item">
-              <img src="${data.sprites.front_default}" alt="">
+            `<li class="list-item">
+              <img src="${data.sprites.front_default}" alt="pokemon">
               <p>${pokemon.name}</p>
             </li>`
           results.insertAdjacentHTML("beforeend", listPokemon)
