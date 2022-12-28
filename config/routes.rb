a403bff9f28f56a8a1f6b363e5abb632e0dfd828
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-12-28 10:05:13 +0100

    controller et model pokemon fait

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home" #défini la page d'acceuil
+	resources :pokemons, only: [:index, :show]
 end
