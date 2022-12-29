27b6533e0f68863ec5385c749cf69df460bd9c61
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-12-29 16:04:15 +0100

    banner

---
+++ app/javascript/animation_card.js
@@ -0,0 +1,29 @@
+const cards = document.querySelectorAll("#card")
+const THRESHOLD = 10;
+
+
+cards.forEach((card) => {
+
+  const handleHover = (e) => {
+    const { clientX, clientY, currentTarget } = e;
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+    const horizontal = (clientX - offsetLeft) / clientWidth;
+    const vertical = (clientY - offsetTop) / clientHeight;
+
+    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+
+    card.style.transform =
+      `perspective(${1000}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg)`;
+  }
+
+  const resetStyles = (e) => {
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  }
+
+  card.addEventListener("mousemove", handleHover);
+  card.addEventListener("mouseleave", resetStyles);
+
+});
--- app/javascript/list_pokemon.js
+++ app/javascript/list_pokemon.js
@@ -1,24 +1,24 @@
-const results = document.querySelector("#list")
+// const results = document.querySelector("#list")
 
-fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
-  .then(response => response.json())
-  .then((data) => {
-    data.results.forEach((pokemon) => {
+// fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
+//   .then(response => response.json())
+//   .then((data) => {
+//     data.results.forEach((pokemon) => {
 
-      fetch(pokemon.url)
-        .then(response => response.json())
-        .then((data) => {
+//       fetch(pokemon.url)
+//         .then(response => response.json())
+//         .then((data) => {
 
-          const listPokemon =
-            `<li class="list-item">
-              <img src="${data.sprites.front_default}" alt="pokemon">
-              <p>${pokemon.name}</p>
-            </li>`
-          results.insertAdjacentHTML("beforeend", listPokemon)
+//           const listPokemon =
+//             `<li class="list-item">
+//               <img src="${data.sprites.front_default}" alt="pokemon">
+//               <p>${pokemon.name}</p>
+//             </li>`
+//           results.insertAdjacentHTML("beforeend", listPokemon)
 
-    })
-  })
-})
+//     })
+//   })
+// })
 
 
 
