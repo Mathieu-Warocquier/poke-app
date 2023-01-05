54deabed430a78b648250efe146d6cdc92c4b065
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-05 17:05:11 +0100

    tjrs le problème de la search

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,10 +1,23 @@
 class PokemonsController < ApplicationController
+  # def index
+  #   # @pokemons = Pokemon.all
+  #   if params[:query].present?
+  #     @pokemons = Pokemon.where('name ILIKE ?', "%#{params[:query]}%")
+  #       else
+  #     @pokemons = Pokemon.all
+  #   end
+
+  #   respond_to do |format|
+  #     format.html # Follow regular flow of Rails
+  #     format.text { render partial: 'list.html', locals: { pokemons: @pokemons } }
+  #   end
+  # end
+
   def index
-    # @pokemons = Pokemon.all
+    @pokemons = Pokemon.order(order: :asc)
+
     if params[:query].present?
-      @pokemons = Pokemon.where('name ILIKE ?', "%#{params[:query]}%")
-        else
-      @pokemons = Pokemon.all
+      @pokemons = @pokemons.where('name ILIKE ?', "%#{params[:query]}%")
     end
 
     respond_to do |format|
--- app/javascript/controllers/refresh_list_controller.js
+++ app/javascript/controllers/refresh_list_controller.js
@@ -5,14 +5,11 @@ export default class extends Controller {
   static targets = [ "form", "list", "searchInput" ]
 
   connect() {
-    console.log(this.element);
-    console.log(this.formTarget);
-    console.log(this.listTarget);
-    console.log(this.searchInputTarget);
+
   }
 
-  update(event) {
-    event.preventDefault();
+  update() {
+
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
