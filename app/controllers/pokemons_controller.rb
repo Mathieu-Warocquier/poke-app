83923adeab7f4973a5196edbe137e46731f974f0
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-06 11:14:21 +0100

    ok

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,17 +1,4 @@
 class PokemonsController < ApplicationController
-  # def index
-  #   # @pokemons = Pokemon.all
-  #   if params[:query].present?
-  #     @pokemons = Pokemon.where('name ILIKE ?', "%#{params[:query]}%")
-  #       else
-  #     @pokemons = Pokemon.all
-  #   end
-
-  #   respond_to do |format|
-  #     format.html # Follow regular flow of Rails
-  #     format.text { render partial: 'list.html', locals: { pokemons: @pokemons } }
-  #   end
-  # end
 
   def index
     @pokemons = Pokemon.order(order: :asc)
