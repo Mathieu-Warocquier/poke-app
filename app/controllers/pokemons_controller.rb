4cfc8d556f2ea0d07106dc606852d6b6e4fa6398
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-01-11 11:44:17 +0100

    problème a fixer sur la home

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,16 +1,17 @@
 class PokemonsController < ApplicationController
 
   def index
+    # @pokemons = Pokemon.all
     @pokemons = Pokemon.order(order: :asc)
 
     if params[:query].present?
       @pokemons = @pokemons.where('name ILIKE ?', "%#{params[:query]}%")
     end
 
-    respond_to do |format|
-      format.html # Follow regular flow of Rails
-      format.text { render partial: 'list.html', locals: { pokemons: @pokemons } }
-    end
+    # respond_to do |format|
+    #   format.html # Follow regular flow of Rails
+    #   format.text { render partial: 'list.html', locals: { pokemons: @pokemons } }
+    # end
   end
 
   def show
